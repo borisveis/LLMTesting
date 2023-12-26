ba1cf0697a24bad9e12d63eda2f32687a4c10948
Boris Veis <boris.veis@gmail.com>
2023-12-26 14:37:50 -0800

    works with config json file

--- src/helloLLM.py
+++ src/helloLLM.py
@@ -1,5 +1,12 @@
+import json
 from gpt4all import GPT4All
-model = GPT4All("orca-mini-3b-gguf2-q4_0.gguf")
-print(model.generate("hello world", max_tokens=100))
+import config
+model = GPT4All(config.model)
+#
+# # entries = os.listdir('.')
+# # print(entries)
+# testconfig = config['testconfig']
+print(model.generate("hello world. My name is Boris", max_tokens=100))
 print(model.generate("how do you say hello in french", max_tokens=100))
+
 print(model.generate("how old are you`", max_tokens=50))
