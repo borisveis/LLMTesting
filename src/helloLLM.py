e4b63494cb0d3dbae41a45f532e00390400ba656
Boris Veis <boris.veis@gmail.com>
2023-12-26 19:08:34 -0800

    simularity2

--- src/helloLLM.py
+++ src/helloLLM.py
@@ -7,6 +7,6 @@ model = GPT4All(config.model)
 # # entries = os.listdir('.')
 responsestates = testFramwork.chat("how many states are in US", 100)
 print("response:"+responsestates)
-numberofstates = "I think like 50"
-print("simularity score")
+numberofstates = "I think there are 50"
+print("cosine simularity score:")
 print(testFramwork.validate.cosine_similarity_score(responsestates,numberofstates))
