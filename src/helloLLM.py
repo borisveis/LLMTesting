913d72a2bcf2dbfe19b3dca8c6a5c84bacece07f
Boris Veis <boris.veis@gmail.com>
2023-12-26 19:46:21 -0800

    cosinesimularity as test

--- src/helloLLM.py
+++ src/helloLLM.py
@@ -7,6 +7,10 @@ model = GPT4All(config.model)
 # # entries = os.listdir('.')
 responsestates = testFramwork.chat("how many states are in US", 100)
 print("response:"+responsestates)
-numberofstates = "I think there are 50"
+numberofstates = "There are 50 states"
 print("cosine simularity score:")
+cosinesimularity = testFramwork.validate.cosine_similarity_score(responsestates,numberofstates)
 print(testFramwork.validate.cosine_similarity_score(responsestates,numberofstates))
+def test_cosinesimularity():
+        print("cosinesimularity="+str(cosinesimularity))
+        assert cosinesimularity > 0.5
