145ffab8283496c9ba9aec2a735e0bca34287a5a
Boris Veis <boris.veis@gmail.com>
2023-12-26 16:37:07 -0800

    simularity

---
+++ src/Similarity.py
@@ -0,0 +1,3 @@
+from gpt4all import GPT4All
+import testFramwork
+print(testFramwork.chat("Hello", 20))
--- src/helloLLM.py
+++ src/helloLLM.py
@@ -1,12 +1,12 @@
 import json
+import testFramwork
 from gpt4all import GPT4All
 import config
 model = GPT4All(config.model)
 #
 # # entries = os.listdir('.')
-# # print(entries)
-# testconfig = config['testconfig']
-print(model.generate("hello world. My name is Boris", max_tokens=100))
-print(model.generate("how do you say hello in french", max_tokens=100))
-
-print(model.generate("how old are you`", max_tokens=50))
+responsestates = testFramwork.chat("how many states are in US", 100)
+print("response:"+responsestates)
+numberofstates = "I think like 50"
+print("simularity score")
+print(testFramwork.validate.cosine_similarity_score(responsestates,numberofstates))
