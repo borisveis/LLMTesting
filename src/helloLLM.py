a76d3bd05a017e1a4ebe44ad07916b8d437ba081
Boris Veis <boris.veis@gmail.com>
2024-02-11 21:52:14 -0800

    cleanup

--- src/helloLLM.py
+++ src/helloLLM.py
@@ -1,4 +1,3 @@
-import json
 import testFramwork
 from gpt4all import GPT4All
 import config
@@ -11,14 +10,13 @@ numberofstates = "There are 50 states"
 print("cosine simularity score:")
 cosinesimularity = testFramwork.validate.cosine_similarity_score(responsestates,numberofstates)
 print(testFramwork.validate.cosine_similarity_score(responsestates,numberofstates))
+
+
 def test_cosinesimularity():
+
         print("cosinesimularity="+str(cosinesimularity))
-
-
 assert cosinesimularity > 0.5
 assert (responsestates.__contains__("United States") and responsestates.__contains__("50"))
 
-
 def test_contains():
-
         assert (responsestates.__contains__("United States") and responsestates.__contains__("50"))
