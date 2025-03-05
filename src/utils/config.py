a2aab1646691620c88a588b8438ab02cde5514ed
Boris Veis <boris.veis@gmail.com>
2025-03-05 12:34:01 -0800

    config and framework tests

--- src/utils/config.py
+++ src/utils/config.py
@@ -11,7 +11,13 @@ config_path = os.path.join(current_dir, 'config.json')
 # Load the config file
 with open(config_path, 'r') as file:
     import json
+
     config = json.load(file)
 
-model= config['testconfig']['model']
-print("using model:"+model)
+
+def get_default_model_name():
+    return config['testconfig']['model']
+
+
+model = get_default_model_name()
+print("using model:" + model)
--- src/utils/test_utils.py
+++ src/utils/test_utils.py
@@ -1,5 +1,16 @@
-from src.utils import framework
+from src.utils import framework, config
 
 
+# framework methods
 def test_cos_sim():
-    assert framework.cosine_similarity_score("abc","abc")==1
+    assert framework.cosine_similarity_score("abc", "abc") == 1
+    assert framework.cosine_similarity_score("my name is Mike", "my name is Don") < 1
+
+
+def test_chat():
+    prompt1 = "how many states in usa?"
+    response = framework.chat(prompt1, 10)
+    assert response.__contains__("50 states")
+        # config methods
+    print(config.get_default_model_name())
+    assert config.get_default_model_name() is not None
